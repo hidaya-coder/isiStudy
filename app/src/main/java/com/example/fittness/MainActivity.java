41ee5f60b2fe6bf68e4c28b46a20b3363fd53eb4
hidaya-coder <hidaya.tabben@etudiant-isi.utm.tn>
2025-09-28 18:12:12 +0100

    finished the onboarding page

--- app/src/main/java/com/example/fittness/MainActivity.java
+++ app/src/main/java/com/example/fittness/MainActivity.java
@@ -17,9 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button next = (Button) findViewById(R.id.start);
 
-        next.setOnClickListener(
+        TextView title = findViewById(R.id.title);
+        String titleText = "<font color='#236DF6'>Isi</font><font color='#141414'>Study</font>";
+        title.setText(Html.fromHtml(titleText));
+
+        TextView subtitle = findViewById(R.id.subtitle);
+        String subtitleText = "Let’s Go To <br>Pomodoro <br><font color='#236DF6'>Study</font> Time.";
+        subtitle.setText(Html.fromHtml(subtitleText));
+
+        Button start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
