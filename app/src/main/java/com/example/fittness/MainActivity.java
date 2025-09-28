66a1e9ada071ff29c4444c371142d516e3729b8a
hidaya-coder <hidaya.tabben@etudiant-isi.utm.tn>
2025-09-28 13:56:27 +0100

    adjusted a few changes

--- app/src/main/java/com/example/fittness/MainActivity.java
+++ app/src/main/java/com/example/fittness/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Button next = (Button) findViewById(R.id.start);
 
